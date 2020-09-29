7848fca0cf1287aff138e410961a4c00c2c437e0
taehoshino <wildsheepchase0211@gmail.com>
2020-09-29 10:11:21 +1000

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, "../public"))
 
 const app = express()
+const port = process.env.PORT || 3000 //use environmental variable value provided by heroku or port=3000 if run locally
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public")
@@ -108,6 +109,6 @@ app.get("*", (req, res)=>{
     })
 })
 
-app.listen(3000, ()=>{
-    console.log("Server is up on port 3000")
+app.listen(port, ()=>{
+    console.log("Server is up on port "+port)
 })
